3ede4ca7ebbf779f97f34f1e7fbc98c933d7adc7
Gaston Snaider <gastonsnaider93@gmail.com>
2017-11-26 12:53:15 -0300

    Add empty rules, facts and queries

---
+++ feedlot.pl
@@ -0,0 +1,28 @@
+% Rules:
+initialAdaptation :- 
+day6Adaptation :-
+day15Adaptation :-
+day26Adaptation :-
+
+initialPercetages :-
+day11Percentages :-
+day22Percentages :-
+
+cold :-
+hot :-
+normalTemperature :-
+
+%Facts
+heads(30)
+day(18)
+initialWeight(225)
+temperature(26)
+
+%Queries
+query(dryFood)
+query(water)
+
+query(sorghum)
+query(corn)
+query(soyExpeller)
+query(mineralPremix)
