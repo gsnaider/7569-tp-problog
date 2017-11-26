6c8373c47a0001aa9be13fe759a14ceb3f6b157c
Gaston Snaider <gastonsnaider93@gmail.com>
2017-11-26 14:34:24 -0300

    Add temperature model

--- feedlot/feedlot.py
+++ feedlot/feedlot.py
@@ -1,8 +1,17 @@
 from problog.program import PrologString
 from problog import get_evaluatable
 
-model = """
-0.3::a.  query(a).
+temperature_model = """
+cold :- temperature(T), T<20.
+hot :- temperature(T), T>28.
+normalTemperature :- \+cold, \+hot.
+
+temperature(10).
+
+query(cold).
+query(hot).
+query(normalTemperature).
 """
-result = get_evaluatable().create_from(PrologString(model)).evaluate()
+
+result = get_evaluatable().create_from(PrologString(temperature_model)).evaluate()
 print result
