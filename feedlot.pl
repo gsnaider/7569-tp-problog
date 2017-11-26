0eec215a5d370b5bfcbaa287f47804d15a1ecfa1
Gaston Snaider <gastonsnaider93@gmail.com>
2017-11-26 13:18:18 -0300

    Add initial rules

--- feedlot.pl
+++ feedlot.pl
@@ -1,16 +1,16 @@
 % Rules:
-initialAdaptation :- 
-day6Adaptation :-
-day15Adaptation :-
-day26Adaptation :-
+initialAdaptation :- day(D), D<6.
+day6Adaptation :- day(D), D>=6, D<15.
+day15Adaptation :- day(D), D>=15, D<26.
+day26Adaptation :- day(D), D>=26.
 
-initialPercetages :-
-day11Percentages :-
-day22Percentages :-
+initialPercetages :- day(D), D<11.
+day11Percentages :- day(D), D>=11, D<22.
+day22Percentages :- day(D), D>=22.
 
-cold :-
-hot :-
-normalTemperature :-
+cold :- temperature(T), T<20.
+hot :- temperature(T), T>28.
+normalTemperature :- \+cold, \+hot.
 
 %Facts
 heads(30)
