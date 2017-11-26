bfeedf76ececb8a17021a76c795848417d3f0127
Gaston Snaider <gastonsnaider93@gmail.com>
2017-11-26 15:21:03 -0300

    Extract all rules to python

--- feedlot/feedlot.py
+++ feedlot/feedlot.py
@@ -13,6 +13,32 @@ query(hot).
 query(normalTemperature).
 """
 
+dry_food_model = """
+initialDryFood :- day(D), D<6.
+day6DryFood :- day(D), D>=6, D<15.
+day15DryFood :- day(D), D>=15, D<26.
+day26DryFood :- day(D), D>=26.
+
+day(18).
+
+query(initialDryFood).
+query(day6DryFood).
+query(day15DryFood).
+query(day26DryFood).
+"""
+
+food_percentages_model = """
+initialPercentages :- day(D), D<11.
+day11Percentages :- day(D), D>=11, D<22.
+day22Percentages :- day(D), D>=22.
+
+day(18).
+
+query(initialPercentages).
+query(day11Percentages).
+query(day22Percentages).
+"""
+
 
 def evaluate_model(model):
     result = get_evaluatable().create_from(PrologString(model)).evaluate()
@@ -24,3 +50,12 @@ def evaluate_model(model):
 
 temperatures = evaluate_model(temperature_model)
 print temperatures
+
+dry_food = evaluate_model(dry_food_model)
+print dry_food
+
+food_percentages = evaluate_model(food_percentages_model)
+print food_percentages
+
+
+
